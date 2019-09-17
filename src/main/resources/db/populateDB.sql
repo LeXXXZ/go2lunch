4759c2800d58ac6f6322bb11eacd7462718399b8
LeXXXZ <lexxxz88@gmail.com>
2019-09-17 03:44:04 +0300

    Rewrite populateDB script with fixed IDs to avoid DB conflicts

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -5,68 +5,50 @@ DELETE FROM menu_dish;
 DELETE FROM dishes;
 DELETE FROM menus;
 DELETE FROM restaurants;
-ALTER SEQUENCE GLOBAL_SEQ RESTART WITH 100000;
+ALTER SEQUENCE GLOBAL_SEQ RESTART WITH 100100;
 
-INSERT INTO users (name, email, password) VALUES
-  ('User', 'user@yandex.ru', 'password'),
-  ('Admin', 'admin@gmail.com', 'admin');
+INSERT INTO users (id, name, email, password) VALUES
+(100000, 'User', 'user@yandex.ru', 'password'),
+(100001, 'Admin', 'admin@gmail.com', 'admin');
 
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001),
   ('ROLE_USER', 100001);
 
-INSERT INTO  restaurants (name) VALUES
--- 100002
-('MacDonalds'),
--- 100003
-('KFC');
+INSERT INTO  restaurants (id, name) VALUES
+(100010, 'MacDonalds'),
+(100011, 'KFC');
 
-INSERT INTO menus (date, rest_id) VALUES
--- 100004
-('2019-08-16', 100002),
--- 100005
-('2019-08-16', 100003),
--- 100006
-(today(), 100002),
--- 100007
-(today(), 100003);
+INSERT INTO menus (id, date, rest_id) VALUES
+(100021, '2019-08-16', 100010),
+(100022, '2019-08-16', 100011),
+(100023, today(), 100010),
+(100024, today(), 100011);
 
-INSERT INTO dishes (name, price, rest_id) VALUES
--- 100016
-('BigMac', 2000, 100002),
--- 100017
-('CheeseBurger', 1000, 100002),
--- 100018
-('Fries', 1000, 100002),
--- 100019
-('BigMac2', 1000, 100002),
--- 100020
-('Twister', 1000, 100003),
--- 100021
-('BoxMaster', 1000, 100003),
--- 100022
-('Twister2', 1000, 100003),
--- 100023
-('BoxMaster2', 1000, 100003);
+INSERT INTO dishes (id, name, price, rest_id) VALUES
+(100030, 'BigMac', 2000, 100010),
+(100031, 'CheeseBurger', 1000, 100010),
+(100032, 'Fries', 1000, 100010),
+(100033, 'BigMac2', 1000, 100010),
+(100034, 'Twister', 1000, 100011),
+(100035, 'BoxMaster', 1000, 100011),
+(100036, 'Twister2', 1000, 100011),
+(100037, 'BoxMaster2', 1000, 100011);
 
-INSERT INTO votes (date, user_id, rest_id) VALUES
--- 100024
-('2019-08-16', 100000, 100002),
--- 100025
-(today(), 100000, 100003),
--- 100026
-('2019-08-16', 100001, 100002),
--- 100027
-(today(), 100001, 100002);
+INSERT INTO votes (id, date, user_id, rest_id) VALUES
+(100040, '2019-08-16', 100000, 100010),
+(100041, today(), 100000, 100011),
+(100042, '2019-08-16', 100001, 100010),
+(100043, today(), 100001, 100011);
 
 INSERT INTO MENU_DISH (menu_id, dish_id) VALUES
-(100004, 100016),
-(100004, 100017),
-(100005, 100020),
-(100005, 100021),
-(100006, 100017),
-(100006, 100018),
-(100006, 100019),
-(100007, 100022),
-(100007, 100023);
+(100021, 100030),
+(100021, 100031),
+(100022, 100034),
+(100022, 100035),
+(100023, 100030),
+(100023, 100032),
+(100023, 100033),
+(100024, 100036),
+(100024, 100037);
